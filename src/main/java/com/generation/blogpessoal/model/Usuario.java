5302bff685dfab7810735c834ad89dc55fc21615
Priscila Lins <priscila_alins@hotmail.com>
2025-10-25 09:17:30 -0300

    Alterações temporárias na branch test

--- src/main/java/com/generation/blogpessoal/model/Usuario.java
+++ src/main/java/com/generation/blogpessoal/model/Usuario.java
@@ -4,32 +4,21 @@ import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.CascadeType;
-
 import jakarta.persistence.Entity;
-
 import jakarta.persistence.FetchType;
-
 import jakarta.persistence.GeneratedValue;
-
 import jakarta.persistence.GenerationType;
-
 import jakarta.persistence.Id;
-
 import jakarta.persistence.OneToMany;
-
 import jakarta.persistence.Table;
-
 import jakarta.validation.constraints.Email;
-
 import jakarta.validation.constraints.NotBlank;
-
 import jakarta.validation.constraints.Size;
 
 @Entity
-
 @Table(name = "tb_usuarios")
-
 public class Usuario {
 
 	@Id
@@ -39,6 +28,7 @@ public class Usuario {
 	@NotBlank(message = "O Atributo Nome é Obrigatório!")
 	private String nome;
 
+	@Schema(example = "email@email.com.br")
 	@NotBlank(message = "O Atributo Usuário é Obrigatório!")
 	@Email(message = "O Atributo Usuário deve ser um email válido!")
 	private String usuario;
